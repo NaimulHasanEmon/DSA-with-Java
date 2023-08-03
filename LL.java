0e0d6e4d9136f63c77a95e32602027c45141a314
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-03 16:22:21 +0600

    Add files via upload

---
+++ LL.java
@@ -0,0 +1,58 @@
+public class LL {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty.\n");
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst(3);
+        list.addFirst(2);
+        list.addFirst(1);
+        list.addLast(4);
+        list.addLast(5);
+        list.printList();
+    }
+}

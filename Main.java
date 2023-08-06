18673e960bdd35b3849798207e8d6db27599ce47
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-06 16:26:43 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        int n = 4, m = 1;
+        for (int i = 0; i < n; i++) {
+            list.add(i, m);
+            m++;
+        }
+        System.out.println("List: " + list);
+        LinkedList<Integer> list2 = new LinkedList<>();
+        int j = list.size() - 1;
+        for (int i = 0; i < n; i++) {
+            list2.add(i, list.get(j));
+            j--;
+        }
+        System.out.println("Reversed List: " + list2);
+
+        // Time complexity O(n);
+    }
+}

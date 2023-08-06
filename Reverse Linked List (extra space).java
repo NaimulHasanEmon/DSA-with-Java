04b1809439c5686e8fd5039b55bd0c5f5bda6062
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-06 16:38:18 +0600

    Update Reverse Linked List (extra space).java

--- Reverse Linked List (extra space).java
+++ Reverse Linked List (extra space).java
@@ -1,12 +1,14 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many elements you want to add: ");
+        int n = sc.nextInt();
         LinkedList<Integer> list = new LinkedList<>();
-        int n = 4, m = 1;
         for (int i = 0; i < n; i++) {
-            list.add(i, m);
-            m++;
+            list.add(i, sc.nextInt());
         }
         System.out.println("List: " + list);
         LinkedList<Integer> list2 = new LinkedList<>();
@@ -17,6 +19,6 @@ class Main {
         }
         System.out.println("Reversed List: " + list2);
 
-        // Time complexity O(n) and extra spaces;
+        // Time complexity O(n);
     }
 }

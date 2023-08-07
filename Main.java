188bbfca5bfde25de5bb0f86590db1b7c2afe27e
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-08 01:50:20 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,66 @@
+class Main {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    Node reverseList(Node head) {
+        if (head == null || head.next == null)
+            return head;
+        Node newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    void traverseList() {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        Main list = new Main();
+        list.addFirst(2);
+        list.addFirst(1);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+        list.traverseList();
+        list.head = list.reverseList(list.head);
+        list.traverseList();
+    }
+}

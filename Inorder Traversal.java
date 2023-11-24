b7cfdc5ef9f88e7c5b848930f2d55c1c760969ad
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-11-24 11:45:46 +0600

    Update and rename Solution.java to Inorder Traversal.java

--- Inorder Traversal.java
+++ Inorder Traversal.java
@@ -25,14 +25,6 @@ public class Solution {
         }
     }
 
-    public static void preorder(Node root) {
-        if (root == null)
-            return;
-        System.out.print(root.data + " ");
-        preorder(root.left);
-        preorder(root.right);
-    }
-
     public static void inorder(Node root) {
         if (root == null)
             return;
@@ -41,23 +33,11 @@ public class Solution {
         inorder(root.right);
     }
 
-    public static void postorder(Node root) {
-        if (root == null)
-            return;
-        postorder(root.left);
-        postorder(root.right);
-        System.out.print(root.data + " ");
-    }
-
     public static void main(String[] args) {
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.print("Preorder traversal: ");
-        preorder(root);
-        System.out.print("\nInorder traversal: ");
+        System.out.print("Inorder traversal: ");
         inorder(root);
-        System.out.print("\nPostorder traversal: ");
-        postorder(root);
     }
 }
